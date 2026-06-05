7499845d69a975ea32caef4d25c3033759bc6a7a
iketsui <ihinatashouyou@gmail.com>
2026-06-05 08:28:25 +0300

    first successful init

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use image::{DynamicImage, ImageReader, Rgb};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let img = ImageReader::open("src/qais.png")?.decode()?;
+    let mut buffer = img.clone().into_rgb8();
+    let dither = 160;
+
+  //  for pixel in buffer.enumerate_pixels_mut() {
+    //  if  *pixel.2 < (150,150,150) {
+      //  *pixel.2 = image::Rgb([255,255,255]);
+      //}   
+    //}
+
+    for (x,y,pixel) in buffer.enumerate_pixels_mut(){
+
+        if (pixel[0] < dither) && (pixel[1] < dither) && (pixel[2] < dither){
+            *pixel = Rgb([0,0,0]);
+
+        }else{
+            *pixel = Rgb([255,255,255])
+        }
+    }
+    println!("{:?}", buffer);
+
+    buffer.save("mqyas.png").unwrap();
+    println!("image saved");
+    Ok(())
+}
