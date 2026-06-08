75f7770bf2ed8731a77ef23901ed1af3780dbc9a
iketsui <ihinatashouyou@gmail.com>
2026-06-08 07:17:01 +0300

    handling color pixels instead of hard pushing white and black pixels

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-use image::{ImageReader, Rgb, RgbImage};
+use image::{ImageReader, Rgb, RgbImage, GrayImage};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let import_img = ImageReader::open("src/qais.png")?.decode()?;
     let mut buffer = import_img.clone().into_rgb8();
-    let dither = 135;
+    let threshold = 100;
+    let factor = 4;
     let (width,height) = buffer.dimensions();
     let mut img = RgbImage::new(width,height);
-    let method = 0;
+    let method = 1;
+
 
     if method == 1 {
         for (x,y,pixel) in buffer.enumerate_pixels_mut(){
-            if (pixel[0] < dither) && (pixel[1] < dither) && (pixel[2] < dither){
-                img.put_pixel(x,y,Rgb([0,0,0]));
-            }else{
-                img.put_pixel(x,y,Rgb([255,255,255]));
-            }
+            let mut r = if pixel[0] < threshold {0} else{255}; 
+            let mut g = if pixel[1] < threshold {0} else{255}; 
+            let mut b = if pixel[2] < threshold {0} else{255}; 
+
+            println!("{}", r);
+
+            img.put_pixel(x,y,Rgb([r,g as u8 ,b as u8]));
         }
         img.save("mqyas.png").unwrap();
         println!("image saved");
