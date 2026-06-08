0dabea23e07ceb93c00e7650623c033c5fb4451d
iketsui <ihinatashouyou@gmail.com>
2026-06-08 05:28:14 +0300

    checkpoint adjustments

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-use image::{DynamicImage, ImageReader, Rgb};
+use image::{ImageReader, Rgb, RgbImage};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let img = ImageReader::open("src/qais.png")?.decode()?;
-    let mut buffer = img.clone().into_rgb8();
-    let dither = 160;
+    let import_img = ImageReader::open("src/qais.png")?.decode()?;
+    let mut buffer = import_img.clone().into_rgb8();
+    let dither = 135;
+    let (width,height) = buffer.dimensions();
+    let mut img = RgbImage::new(width,height);
+    let method = 0;
 
-    for (x,y,pixel) in buffer.enumerate_pixels_mut(){
-
-        if (pixel[0] < dither) && (pixel[1] < dither) && (pixel[2] < dither){
-            *pixel = Rgb([0,0,0]);
-
-        }else{
-            *pixel = Rgb([255,255,255])
+    if method == 1 {
+        for (x,y,pixel) in buffer.enumerate_pixels_mut(){
+            if (pixel[0] < dither) && (pixel[1] < dither) && (pixel[2] < dither){
+                img.put_pixel(x,y,Rgb([0,0,0]));
+            }else{
+                img.put_pixel(x,y,Rgb([255,255,255]));
+            }
         }
+        img.save("mqyas.png").unwrap();
+        println!("image saved");
     }
-    println!("{:?}", buffer);
 
-    buffer.save("mqyas.png").unwrap();
-    println!("image saved");
+    if method == 2 {
+
+    }
+
     Ok(())
 }
